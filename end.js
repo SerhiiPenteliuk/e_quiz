aa34b45d15c118931777abebf5b10effd90b6f47
SerhiiPenteliuk <sergunyapw@gmail.com>
2022-11-17 17:06:13 +0200

    c

--- end.js
+++ end.js
@@ -2,12 +2,19 @@ const username = document.getElementById('username');
 const saveScoreBtn = document.getElementById('saveScoreBtn');
 const finalScore = document.getElementById('finalScore');
 const mostRecentScore = localStorage.getItem('mostRecentScore');
+let yourResult = document.getElementById('yourResult');
 
 const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
 
 const MAX_HIGH_SCORES = 5;
 
-finalScore.innerText = mostRecentScore;
+if (mostRecentScore >= 0 && mostRecentScore <= 3) {
+    yourResult.innerText = 'Ти справжній бек'
+} else if (mostRecentScore >= 4 && mostRecentScore <=6) {
+    yourResult.innerText = 'Да ти фулстек!'
+} else {
+    yourResult.innerText = 'Вітаю, ти фронт!'
+}
 
 username.addEventListener('keyup', () => {
     saveScoreBtn.disabled = !username.value;
