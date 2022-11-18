55a34af9ef63c98acefb4ae58c90758322fcea63
SerhiiPenteliuk <sergunyapw@gmail.com>
2022-11-18 15:19:32 +0200

    Release 1.0.0

--- end.js
+++ end.js
@@ -4,9 +4,20 @@ let resultLink = document.getElementById('resultLink');
 
 
 if (mostRecentScore >= 0 && mostRecentScore <= 3) {
+    //текст заголовку
     yourResult.innerText = 'На жаль, Ти справжній бек'
-} else if (mostRecentScore >= 4 && mostRecentScore <=6) {
+    //текст посилання
+    resultLink.innerText = 'Тримай лінку для бека'
+    //лінк
+    resultLink.setAttribute(
+        'href', 'https://github.com/SerhiiPenteliuk/e_quiz/tree/main'
+    )
+} else if (mostRecentScore >= 4 && mostRecentScore <= 6) {
     yourResult.innerText = 'Да ти фулстек!'
+    resultLink.innerText = 'Тримай лінку для'
+    resultLink.setAttribute('href', '#')
 } else {
     yourResult.innerText = 'Вітаю, Ти фронт!'
+    resultLink.innerText = 'Тримай лінку для'
+    resultLink.setAttribute('href', '#');
 }
