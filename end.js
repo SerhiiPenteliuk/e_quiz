189fb47e85c7b219525da6accab3c7e2790865c9
SerhiiPenteliuk <sergunyapw@gmail.com>
2022-11-20 13:44:18 +0200

    Release 2.2.0

--- end.js
+++ end.js
@@ -1,26 +1,20 @@
 const mostRecentScore = localStorage.getItem('mostRecentScore');
 let yourResult = document.getElementById('yourResult');
 let hru = document.getElementById('hru')
-let resultLink = document.getElementById('resultLink');
+let addInfo = document.getElementById('add-info');
 
 if (mostRecentScore >= 0 && mostRecentScore <= 3) {
     //текст заголовку
     yourResult.innerText = 'Ти справжній Back-end Developer!'
     //текст посилання
-    resultLink.innerText = 'Реєструйся та спробуй свої сили на нашому курсі Magento BE Dev'
-    //лінк
-    resultLink.setAttribute(
-        'action', 'https://forms.gle/zPT7H7b9DAjYH7yv6'
-    )
+    addInfo.innerText = 'Реєструйся та спробуй свої сили на нашому курсі Magento BE Dev'
     hru.setAttribute('href','https://forms.gle/zPT7H7b9DAjYH7yv6')
 } else if (mostRecentScore >= 4 && mostRecentScore <= 6) {
     yourResult.innerText = 'Да ти справжній FullStack Developer!'
-    resultLink.innerText = 'Реєструйся та спробуй свої сили на нашому курсі Sales Force Commerce cloud'
-    resultLink.setAttribute('href', 'https://forms.gle/rNjQtW9zq8K2XdGT6')
+    addInfo.innerText = 'Реєструйся та спробуй свої сили на нашому курсі Sales Force Commerce Cloud'
     hru.setAttribute('href','https://forms.gle/zPT7H7b9DAjYH7yv6')
 } else {
     yourResult.innerText = 'Вітаю, Ти Front-End Developer!'
-    resultLink.innerText = 'Реєструйся та спробуй свої сили на нашому курсі Magento FE Dev'
-    resultLink.setAttribute('href', 'https://forms.gle/7TvN7cZcUYx4jLDM7');
+    addInfo.innerText = 'Реєструйся та спробуй свої сили на нашому курсі Magento FE Dev'
     hru.setAttribute('href','https://forms.gle/zPT7H7b9DAjYH7yv6')
 }
